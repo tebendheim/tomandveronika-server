a336310e8f21b0c5f81dfed7f53c2887ff90ce63
tebendheim <tomelbin94@gmail.com>
2022-06-18 13:24:56 +0200

    prøver igjen

---
+++ express/sendgrid.js
@@ -0,0 +1,24 @@
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+const sendgrid = require('./sendgrid');
+
+
+
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js! sendgrid</h1>');
+  res.end();
+});
+//router.get('/', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/sendgrid', router);  // path must route to lambda
+
+
+module.exports = app;
+module.exports.handler = serverless(app);
