3577f54514c47b3105bdc0c9da902bead1b377c8
tebendheim <tomelbin94@gmail.com>
2022-07-22 00:55:58 +0200

    Cors error på forrige. Lagt til cors på router

--- express/forgotpassword.js
+++ express/forgotpassword.js
@@ -7,12 +7,12 @@ const connect = require('../functions/db');
 const captcha = require('../middleware/captcha');
 const sendMail = require('../functions/sendResetMail');
 require('dotenv').config();
-//const cors = require('cors');
+const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
 router.use(helmet());
 router.use(morgan('combined'));
-/*router.use(
+router.use(
 	cors({
 		origin: '*',
 	})
