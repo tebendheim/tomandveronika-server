3d7a19655a027c0ea72d0c122bdcaf4fe2cc45b0
tebendheim <tomelbin94@gmail.com>
2022-07-22 19:14:41 +0200

    Coprøve å legge til res.header

--- express/api.js
+++ express/api.js
@@ -23,12 +23,10 @@ const test = require('./test');
 require('dotenv').config();
 router.use(helmet());
 router.use(morgan('combined'));
-app.use(function (req, res, next) {
+app.all('*', function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requiseted-With, Content-Type, Accept'
-	);
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
 	next();
 });
 app.use(cors());
