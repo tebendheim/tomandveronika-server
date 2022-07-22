d550f45bb56d2a4270b5622e8cd6327519c97d1f
tebendheim <tomelbin94@gmail.com>
2022-07-22 20:36:23 +0200

    Coprøve å legge til res.header

--- express/forgotpassword.js
+++ express/forgotpassword.js
@@ -2,44 +2,26 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-
-const headers = require('../middleware/headers');
 const connect = require('../functions/db');
 const captcha = require('../middleware/captcha');
-const sendMail = require('../functions/sendResetMail');
 require('dotenv').config();
-const cors = require('cors');
-const morgan = require('morgan');
-const helmet = require('helmet');
-router.use(helmet());
-router.use(morgan('combined'));
 
-/*router.use(
-	cors({
-		origin: '*',
-	})
-); */ //Uten denne vil man få nettwork error. */
-
-// get usermodel
 const User = require('../models/User');
 const Token = require('../models/resetPassword');
 
-router.post('/', headers, cors(), async (req, res) => {
+router.post('/', async (req, res) => {
 	//destructure email from request
 	const { email } = req.body.data;
 	//connecting to database
 	connect();
 	//see if a token exists
 	try {
-		const user = await User.find({ email });
-		res.json(user);
+		//const user = await User.find({ email });
+		res.json('ok');
 		return;
 	} catch (err) {
 		console.log(err);
-		res
-			.status(400)
-			.header('Access-Control-Allow-Origin', '*')
-			.json({ msg: err });
+		res.status(400).json({ msg: err });
 		return;
 	}
 });
