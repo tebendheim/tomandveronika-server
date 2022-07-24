2908eeeca73012c252e23f26e23431e81cb7daa7
tebendheim <tomelbin94@gmail.com>
2022-07-24 23:35:24 +0200

    Trying to fix cors error. cors sett on top of API.js and app.use urlencoded is added

--- express/api.js
+++ express/api.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-//app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use((_, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*'); // or 'localhost:8888'
@@ -23,7 +23,7 @@ app.use(
 		origin: '*',
 	})
 );
-*/
+// Sets headers before routes*/
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const router = express.Router();
