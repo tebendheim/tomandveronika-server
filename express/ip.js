f26db0628ce0b99e32aadef513c3351dfc67c245
tebendheim <tomelbin94@gmail.com>
2022-08-10 19:56:03 +0200

    lprøvd å legge til session på ip

--- express/ip.js
+++ express/ip.js
@@ -8,12 +8,19 @@ const router = express.Router();
 require('dotenv').config();
 const apiLimiter = require('../middleware/ratelimiter');
 router.use(express.json());
+const db = require('../models');
+const Session = db.session;
 
 let count = 0;
 
-router.post('/', (request, response) => {
-	console.log(request);
-	return response.json(request.body);
+router.post('/', (req, res) => {
+	//console.log(req.socket.remoteAddress);
+	const ip = req.headers['x-forwarded-for'];
+	console.log(ip);
+	const session = new Session({ ip: ip });
+	await session.save()
+	res.json('ok?');
+	return;
 });
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -6,4 +6,5 @@ db.user = require('./user.model');
 db.role = require('./role.model');
 db.ROLES = ['user', 'admin', 'moderator'];
 db.resettoken = require('./resettoken.model');
+db.session = require('./session.model');
 module.exports = db;
